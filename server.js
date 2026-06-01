2d2b194c899740680e1476998af8d74e9f82a51b
Sebastian1823 <sebastian.bayona@tecsup.edu.pe>
2026-06-01 16:16:56 -0500

    fix: configurar CORS explícito

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use(logger);
 
@@ -46,7 +50,6 @@ app.use(errorHandler);
 const startServer = async () => {
   try {
     await testConnection();
-
     app.listen(PORT, () => {
       console.log(
         `\x1b[36mServidor corriendo en puerto ${PORT}\x1b[0m`
